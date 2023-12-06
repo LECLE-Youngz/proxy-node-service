07661dd0dc53d5941a7949729f7d85fd2248e722
Kiet1618 <ngokiet3207@gmail.com>
2023-12-07 05:53:34 +0700

    fix

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -17,7 +17,7 @@ describe('AppController (e2e)', () => {
 
   it('/ (GET)', () => {
     return request(app.getHttpServer())
-      .get('/api/v1/ping')
+      .get('/')
       .expect(200)
       .expect('Server is running');
   });
